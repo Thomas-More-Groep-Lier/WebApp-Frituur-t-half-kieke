098fac1b49f4a2ce463099185cd1adbcace4e68d
Maarten De Puysseleir <maartendp@gmail.com>
2022-04-20 10:57:24 +0200

    Entities aangepast, als iemand even kan nakijken zou fijn zijn. voor de bestelling heb ik geen Bereken totaal prijs en toonbestelling toegevoegd gezien die in de repositories moeten komen

--- hethalfkieken/src/main/java/be/thomasmore/hethalfkieken/entities/Client.java
+++ hethalfkieken/src/main/java/be/thomasmore/hethalfkieken/entities/Client.java
@@ -8,5 +8,49 @@ import javax.persistence.Id;
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//attributes
     private long id;
+    private String name;
+    private String phoneNumber;
+    private String emailAdress;
+//getters
+    public long getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getEmailAdress(){
+        return emailAdress
+    }
+//Setters
+    //If setter for Id is never used in the application remove setter
+    public void setId(long newId){
+        this.id = newId
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setPhoneNumber(String newPhoneNumber){
+        this.phoneNumber = newPhoneNumber;
+    }
+    public void setEmailAdress(String newEmailAdress){
+        this.emailAdress = newEmailAdress;
+    }
+//constructors
+    public Client(long id,String name, String phoneNumber, String emailAdress){
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAdress = emailAdress;
+    }
+    //constructor without id
+    public Client(String name, String phoneNumber, String emailAdress){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAdress = emailAdress;
+    }
 }
--- hethalfkieken/src/main/java/be/thomasmore/hethalfkieken/entities/Order.java
+++ hethalfkieken/src/main/java/be/thomasmore/hethalfkieken/entities/Order.java
@@ -4,9 +4,74 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//Attributes
     private long id;
+    private long clientId;
+    private long timeSlotId;
+    //find out how to use date variables seems to be a bit more complicated then c#
+    private String orderDate;
+    private String wantedRetrievalTime;
+    private Bool orderToBig;
+//getters
+    public long getId(){
+        return id;
+    }
+    public long getClientId(){
+        return clientId;
+    }
+    public long getTimeSlotId(){return timeSlotId;}
+    public String getOrderDate(){
+        return orderDate;
+    }
+    public String getWantedRetrievalTime(){
+        return wantedRetrievalTime;
+    }
+    public Bool getOrderToBig() {
+        return OrderToBig;
+    }
+//Setters
+    //If setter for Id is never used in the application remove setter
+    public void setId(long newId){
+        this.id = newId;
+    }
+    public void setClientId(long newClientId){
+        this.clientId = newClientId;
+    }
+    public void setTimeSlotId(long newTimeSlotId){this.timeSlotId = newTimeSlotId;}
+    public void setOrderDate(String newOrderDate){
+        this.orderDate = newOrderDate;
+    }
+    public void setWantedRetrievalTime(String wantedRetrievalTime) {
+        this.wantedRetrievalTime = wantedRetrievalTime;
+    }
+    public void setOrderToBig() {
+        if(orderToBig){
+            orderToBig = false;
+        }
+        else{
+            orderToBig = true;
+        }
+    }
+//constructors
+    public Order(long id, long clientId, String orderDate, String wantedRetrievalTime, Bool orderToBig){
+        this.id = id;
+        this.clientId = clientId;
+        this.orderDate = orderDate;
+        this.wantedRetrievalTime = wantedRetrievalTime;
+        this.orderToBig = orderToBig;
+
+    }
+    //constructor without id
+    public Order(long clientId, String orderDate, String wantedRetrievalTime, Bool orderToBig){
+        this.clientId = clientId;
+        this.orderDate = orderDate;
+        this.wantedRetrievalTime = wantedRetrievalTime;
+        this.orderToBig = orderToBig;
+
+    }
 }
