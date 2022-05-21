2f1cc3a922a10ead4204dd8699a33ab5e4130378
Mitch Van Vlierberghe <80568805+mitch-vvl-thomas-more-student@users.noreply.github.com>
2022-05-21 14:56:16 +0200

    Plaats orders als klant. En selecteer hierbij een tijdslot.

--- hellospring/src/main/java/be/thomasmore/graduaten/hellospring/HellospringApplication.java
+++ hellospring/src/main/java/be/thomasmore/graduaten/hellospring/HellospringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HellospringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellospringApplication.class, args);
+        SpringApplication.run(HellospringApplication.class , args);
     }
 
 }
